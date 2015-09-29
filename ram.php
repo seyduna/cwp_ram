3c209bc84cef9c490df22b211d0d8b26e80a8a48
seyduna <canmesee@hotmail.com>
2015-09-29 22:06:55 +0300

    Create ram.php

---
+++ ram.php
@@ -0,0 +1,33 @@
+<html>
+<body>
+
+<form action="index.php?module=ram" method="post">
+
+Ram: <input type="text" name="ram"><br>
+<br>
+<p> <input type="submit" value="Ayarla"> </p>
+</form>
+
+<?php
+exec('free -mo', $out);
+preg_match_all('/\s+([0-9]+)/', $out[1], $matches);
+list($total, $used, $free, $shared, $buffers, $cached) = $matches[1];
+
+if (empty($_POST["ram"]))
+{
+echo "Lütfen kontrol edilecek RAM miktarını giriniz";}
+else{
+
+else{
+if($used >= $_POST["ram"])
+{
+echo shell_exec("sync && echo 3 > /proc/sys/vm/drop_caches");
+echo "RAM miktarı ".$_POST["ram"]." göre kontrol edildi";
+
+exit();
+}
+}
+?>
+
+</body>
+</html>
